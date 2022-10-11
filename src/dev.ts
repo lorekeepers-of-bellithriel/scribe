18b8705e22f8fda917a8b2fcb9406b94e3ed7c65
obdsf <obdsf0@gmail.com>
2022-10-11 21:56:33 +0300

    0

--- src/dev.ts
+++ src/dev.ts
@@ -1,6 +1,6 @@
 import { LogLevels, Scribe } from "./main.js";
 
-Scribe.configure({ level: LogLevels.All });
+const scribe = new Scribe({ level: LogLevels.All });
 
 console.log("skata");
 
@@ -17,21 +17,21 @@ console.log("skata");
 // console.timeEnd("kappa default-log");
 
 // console.time("kappa scribe-warn");
-// for (let i = 0; i < 1000; i++) Scribe.warn("warn");
+// for (let i = 0; i < 1000; i++) scribe.warn("warn");
 // console.timeEnd("kappa scribe-warn");
 
 // console.time("kappa scribe-error");
-// for (let i = 0; i < 1000; i++) Scribe.error("error");
+// for (let i = 0; i < 1000; i++) scribe.error("error");
 // console.timeEnd("kappa scribe-error");
 
 // console.time("kappa scribe-info");
-// for (let i = 0; i < 1000; i++) Scribe.info("info");
+// for (let i = 0; i < 1000; i++) scribe.info("info");
 // console.timeEnd("kappa scribe-info");
 
 // console.time("kappa scribe-trace");
-// for (let i = 0; i < 1000; i++) Scribe.trace("trace");
+// for (let i = 0; i < 1000; i++) scribe.trace("trace");
 // console.timeEnd("kappa scribe-trace");
 
 // console.time("kappa scribe-inspect");
-// for (let i = 0; i < 1000; i++) Scribe.inspect("inspect");
+// for (let i = 0; i < 1000; i++) scribe.inspect("inspect");
 // console.timeEnd("kappa scribe-inspect");
--- src/main.ts
+++ src/main.ts
@@ -41,57 +41,136 @@ type LogVariantsCollection = {
 
 const disabledLog: Log = () => {};
 
-export type ConfigurationOptions = RequireAtLeastOne<{
-    level: LogLevels;
-    pretty: boolean;
-}>;
-
 type PrefixConstructor = (level: UsableLogLevels, text?: string) => string;
 
+type BaseOptions = {
+    level: LogLevels;
+    pretty: boolean;
+};
+
+export type Options = Partial<BaseOptions>;
+
+export type ConfigurationOptions = RequireAtLeastOne<BaseOptions>;
+
 export class Scribe {
-    private static _level: LogLevels;
-    public static get level() {
+    private _level: LogLevels;
+    public get level() {
         return this._level;
     }
-    private static _pretty: boolean;
-    public static get pretty() {
+    private _pretty: boolean;
+    public get pretty() {
         return this._pretty;
     }
-    private static _logLevelsNameAndColorCollection: LogLevelsNameAndColorCollection = {
-        [LogLevels.Error]: {
-            name: " ERR  ",
-            color: "#F44336",
-        },
-        [LogLevels.Warn]: {
-            name: " WARN ",
-            color: "#FFC107",
-        },
-        [LogLevels.Info]: {
-            name: " INFO ",
-            color: "#4CAF50",
-        },
-        [LogLevels.Trace]: {
-            name: " TRC  ",
-            color: "#00BCD4",
-        },
-        [LogLevels.Inspect]: {
-            name: " INS  ",
-            color: "#9C27B0",
-        },
-    };
-    private static _nodePrefixConstructor: PrefixConstructor = (level: UsableLogLevels, text?: string): string => {
+    private _logLevelsNameAndColorCollection: LogLevelsNameAndColorCollection;
+    private _activeLogVariantsCollection: ActiveLogVariantsCollection;
+    private _logVariantsCollection: LogVariantsCollection;
+    private _error: Log;
+    public get error() {
+        return this._error;
+    }
+    private _warn: Log;
+    public get warn() {
+        return this._warn;
+    }
+    private _info: Log;
+    public get info() {
+        return this._info;
+    }
+    private _trace: Log;
+    public get trace() {
+        return this._trace;
+    }
+    private _inspect: Log;
+    public get inspect() {
+        return this._inspect;
+    }
+    constructor(options: Options) {
+        this._level = is.undefined(options.level) ? LogLevels.Info : options.level;
+        this._pretty = is.undefined(options.pretty) ? true : options.pretty;
+        this._logLevelsNameAndColorCollection = {
+            [LogLevels.Error]: {
+                name: " ERR  ",
+                color: "#F44336",
+            },
+            [LogLevels.Warn]: {
+                name: " WARN ",
+                color: "#FFC107",
+            },
+            [LogLevels.Info]: {
+                name: " INFO ",
+                color: "#4CAF50",
+            },
+            [LogLevels.Trace]: {
+                name: " TRC  ",
+                color: "#00BCD4",
+            },
+            [LogLevels.Inspect]: {
+                name: " INS  ",
+                color: "#9C27B0",
+            },
+        };
+        this._activeLogVariantsCollection = {
+            [LogLevels.Error]: {
+                node: (...args: unknown[]) => this._scribe(LogLevels.Error, this._nodePrefixConstructor, console.error, ...args),
+                browser: (...args: unknown[]) => this._scribe(LogLevels.Error, this._browserPrefixConstructor, console.error, ...args),
+            },
+            [LogLevels.Warn]: {
+                node: (...args: unknown[]) => this._scribe(LogLevels.Warn, this._nodePrefixConstructor, console.warn, ...args),
+                browser: (...args: unknown[]) => this._scribe(LogLevels.Warn, this._browserPrefixConstructor, console.warn, ...args),
+            },
+            [LogLevels.Info]: {
+                node: (...args: unknown[]) => this._scribe(LogLevels.Info, this._nodePrefixConstructor, console.info, ...args),
+                browser: (...args: unknown[]) => this._scribe(LogLevels.Info, this._browserPrefixConstructor, console.info, ...args),
+            },
+            [LogLevels.Trace]: {
+                node: (...args: unknown[]) => this._scribe(LogLevels.Trace, this._nodePrefixConstructor, console.log, ...args),
+                browser: (...args: unknown[]) => this._scribe(LogLevels.Trace, this._browserPrefixConstructor, console.log, ...args),
+            },
+            [LogLevels.Inspect]: {
+                node: (...args: unknown[]) => this._scribe(LogLevels.Inspect, this._nodePrefixConstructor, console.log, ...args),
+                browser: (...args: unknown[]) => this._scribe(LogLevels.Inspect, this._browserPrefixConstructor, console.log, ...args),
+            },
+        };
+        this._logVariantsCollection = {
+            [LogLevels.Error]: isNode
+                ? this._activeLogVariantsCollection[LogLevels.Error].node //
+                : this._activeLogVariantsCollection[LogLevels.Error].browser,
+            [LogLevels.Warn]: isNode
+                ? this._activeLogVariantsCollection[LogLevels.Warn].node //
+                : this._activeLogVariantsCollection[LogLevels.Warn].browser,
+            [LogLevels.Info]: isNode
+                ? this._activeLogVariantsCollection[LogLevels.Info].node //
+                : this._activeLogVariantsCollection[LogLevels.Info].browser,
+            [LogLevels.Trace]: isNode
+                ? this._activeLogVariantsCollection[LogLevels.Trace].node //
+                : this._activeLogVariantsCollection[LogLevels.Trace].browser,
+            [LogLevels.Inspect]: isNode
+                ? this._activeLogVariantsCollection[LogLevels.Inspect].node //
+                : this._activeLogVariantsCollection[LogLevels.Inspect].browser,
+        };
+        this._error = disabledLog;
+        this._warn = disabledLog;
+        this._info = disabledLog;
+        this._trace = disabledLog;
+        this._inspect = disabledLog;
+        this.configure({
+            level: this._level,
+            pretty: this._pretty,
+        });
+    }
+    private _nodePrefixConstructor: PrefixConstructor = (level: UsableLogLevels, text?: string): string => {
         const llnc = this._logLevelsNameAndColorCollection[level];
         let prefix = this._pretty ? chalk.bgHex(llnc.color).bold(llnc.name) : llnc.name;
         if (text !== undefined) prefix += ` ${text} `;
         return prefix;
     };
-    private static _browserPrefixConstructor: PrefixConstructor = (level: UsableLogLevels, text?: string): string => {
+    private _browserPrefixConstructor: PrefixConstructor = (level: UsableLogLevels, text?: string): string => {
         const llnc = this._logLevelsNameAndColorCollection[level];
         let prefix = this._pretty ? `implement` : llnc.name;
         if (text !== undefined) prefix += ` ${text} `;
         return prefix;
     };
-    private static _scribe = (level: UsableLogLevels, prefixConstructor: PrefixConstructor, log: typeof console.log, ...args: unknown[]) => {
+    private _scribe = (level: UsableLogLevels, prefixConstructor: PrefixConstructor, log: typeof console.log, ...args: unknown[]) => {
         let prefix: string;
         let text: string | undefined = undefined;
         const first = args[0];
@@ -102,46 +181,7 @@ export class Scribe {
         prefix = prefixConstructor(level, text);
         log(prefix, ...args);
     };
-    private static _activeLogVariantsCollection: ActiveLogVariantsCollection = {
-        [LogLevels.Error]: {
-            node: (...args: unknown[]) => this._scribe(LogLevels.Error, this._nodePrefixConstructor, console.error, ...args),
-            browser: (...args: unknown[]) => this._scribe(LogLevels.Error, this._browserPrefixConstructor, console.error, ...args),
-        },
-        [LogLevels.Warn]: {
-            node: (...args: unknown[]) => this._scribe(LogLevels.Warn, this._nodePrefixConstructor, console.warn, ...args),
-            browser: (...args: unknown[]) => this._scribe(LogLevels.Warn, this._browserPrefixConstructor, console.warn, ...args),
-        },
-        [LogLevels.Info]: {
-            node: (...args: unknown[]) => this._scribe(LogLevels.Info, this._nodePrefixConstructor, console.info, ...args),
-            browser: (...args: unknown[]) => this._scribe(LogLevels.Info, this._browserPrefixConstructor, console.info, ...args),
-        },
-        [LogLevels.Trace]: {
-            node: (...args: unknown[]) => this._scribe(LogLevels.Trace, this._nodePrefixConstructor, console.log, ...args),
-            browser: (...args: unknown[]) => this._scribe(LogLevels.Trace, this._browserPrefixConstructor, console.log, ...args),
-        },
-        [LogLevels.Inspect]: {
-            node: (...args: unknown[]) => this._scribe(LogLevels.Inspect, this._nodePrefixConstructor, console.log, ...args),
-            browser: (...args: unknown[]) => this._scribe(LogLevels.Inspect, this._browserPrefixConstructor, console.log, ...args),
-        },
-    };
-    private static _logVariantsCollection: LogVariantsCollection = {
-        [LogLevels.Error]: isNode
-            ? this._activeLogVariantsCollection[LogLevels.Error].node //
-            : this._activeLogVariantsCollection[LogLevels.Error].browser,
-        [LogLevels.Warn]: isNode
-            ? this._activeLogVariantsCollection[LogLevels.Warn].node //
-            : this._activeLogVariantsCollection[LogLevels.Warn].browser,
-        [LogLevels.Info]: isNode
-            ? this._activeLogVariantsCollection[LogLevels.Info].node //
-            : this._activeLogVariantsCollection[LogLevels.Info].browser,
-        [LogLevels.Trace]: isNode
-            ? this._activeLogVariantsCollection[LogLevels.Trace].node //
-            : this._activeLogVariantsCollection[LogLevels.Trace].browser,
-        [LogLevels.Inspect]: isNode
-            ? this._activeLogVariantsCollection[LogLevels.Inspect].node //
-            : this._activeLogVariantsCollection[LogLevels.Inspect].browser,
-    };
-    public static configure = (options: ConfigurationOptions) => {
+    public configure = (options: ConfigurationOptions) => {
         let count = 0;
         if (!is.undefined(options.level)) {
             this._level = options.level;
@@ -178,32 +218,4 @@ export class Scribe {
         }
         if (count === 0) this.error(PREFIX, "received no options during configuration");
     };
-    private static _error: Log;
-    public static get error() {
-        return this._error;
-    }
-    private static _warn: Log;
-    public static get warn() {
-        return this._warn;
-    }
-    private static _info: Log;
-    public static get info() {
-        return this._info;
-    }
-    private static _trace: Log;
-    public static get trace() {
-        return this._trace;
-    }
-    private static _inspect: Log;
-    public static get inspect() {
-        return this._inspect;
-    }
-    private static _initialize = (() => {
-        this._level = LogLevels.Info;
-        this._pretty = true;
-        this.configure({
-            level: this._level,
-            pretty: this._pretty,
-        });
-    })();
 }
